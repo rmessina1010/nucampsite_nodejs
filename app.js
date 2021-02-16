d426baa0741a87e6f5f83fad32f2a35cf33ce761
Ray Messina <rmessina10@gmail.com>
2021-02-16 00:02:51 -0600

    File Upload

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
+
 
 
 const mongoose = require('mongoose');
@@ -57,6 +60,7 @@ app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
